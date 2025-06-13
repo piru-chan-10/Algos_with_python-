748fd32d9ac9f65e04bcd66132a3f845dc7149a2
piru-chan-10 <priyanshu792005@gmail.com>
2025-06-13 13:57:44 +0530

    Update XOR.py

--- XOR.py
+++ XOR.py
@@ -1,26 +1,33 @@
 ''' XOR encryption by siroyasha '''
 class XOR_encryptor:
     def __init__(self,message):
-        # converts message to ASCII character 
-        # if yu are using uppercase then make the key also uppercase 
         self.message=[ord(m) for m in message.upper() ]
         self.ciphertext=[] # the encrypted text after xor operatrion
         self.key_str=[] # holds the ascii values of self.key synced with self.message 
         self.key="KEY"
-        # choose a complex key for the message i have chosen 'key' for clarity  
     def _xor(self):
-     
-        key_str=self.key+"".join([self.key[i] for i in range(len(self.message)-len(self.key))])
-        self.key_str=[ord(i) for i in key_str]
-        
+        def match_key(message:str,key:str )->str:
+            assert len(message)>=len(key),'::>>KEY STRING SHOULD NOT BE GREATER THAN THE MESSAGE STRING'
+            l_m,l_k=len(message),len(key)
+            match,pivot=[0,1,2],[i for i in range(len(message)-len(key))]
+            p=0
+            for i in range(len(pivot)):
+                if p<len(message):
+                    key=key+''.join(key[p])
+                    p+=1
+                else:
+                    p=0
+                    key=key+''.join(key[p])
+                    p+=1
+            return key
+        self.key_str=match_key(self.message,self.key)
+        self.key_str=[ord(i) for i in self.key_str]
         self.ciphertext=[self.message[i]^self.key_str[i] for i in range(len(self.message))]
-        return self.ciphertext
+        return self.key_str,self.ciphertext
     def decryption(self):
-        # this methods the encrypted ciphertext 
         decrypt="".join([chr(self.key_str[i]^self.ciphertext[i]) for i in range(len(self.key_str))])
         return decrypt
-        
-if __name__=="__main__":
-  A=XOR_encryptor("hello")
-  A._xor()
-  A.decryption()
+A=XOR_encryptor("priyanshu")
+A.message
+A._xor()
+A.decryption()
